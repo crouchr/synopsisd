db721e9ead2682fa0d553a60f1b096d1cbccd757
crouchr <richard.crouch100@gmail.com>
2021-03-04 14:40:54 +0000

    bug fix

--- app/get_cumulus_weather_info.py
+++ app/get_cumulus_weather_info.py
@@ -1,13 +1,14 @@
-import call_rest_api
+import cumulus_comms
 from pprint import pprint
 
-# This function is common with webcamd - consolidate
+
+# FIXME : This function is common with webcamd - consolidate
 def get_key_weather_variables(cumulus_endpoint):
     """
     Get some critical weather variables by querying the CumulusMX REST API
     """
 
-    status_code, response_dict = call_rest_api.call_rest_api(cumulus_endpoint, None)
+    status_code, response_dict = cumulus_comms.call_rest_api(cumulus_endpoint, None)
 
     # Aercus to CumulusMX serial connection down - all data now invalid
     if status_code == 200 and response_dict['DataStopped'] :
@@ -17,4 +18,3 @@ def get_key_weather_variables(cumulus_endpoint):
         return response_dict
     else:
         return None
-
